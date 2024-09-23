c99693a4b89e6d6227790ecdef124c5dac6cc9fd
teevik <teemu.vikoren@gmail.com>
2024-09-23 15:59:55 +0200

    arbitary language injection with a comment

--- queries/injections.scm
+++ queries/injections.scm
@@ -7,3 +7,8 @@
   (token_tree) @injection.content)
  (#set! injection.language "rust")
  (#set! injection.include-children))
+
+; mark arbitary languages with a comment
+((((comment) @injection.language) .
+  (indented_string_expression (string_fragment) @injection.content))
+  (#set! injection.combined))
